9e1b6e470213289a84de6d920834ee6fe5b5f21e
intellign <ismailarashad@aol.com>
2024-11-23 08:15:51 -0500

    Update modal_sheets.dart

--- lib/src/presentation/utils/modal_sheets.dart
+++ lib/src/presentation/utils/modal_sheets.dart
@@ -1,21 +1,26 @@
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:modal_gif_picker/modal_gif_picker.dart';
 import 'package:provider/provider.dart';
+import 'dart:math' as math;
 import 'package:stories_editor/src/domain/models/editable_items.dart';
 import 'package:stories_editor/src/domain/providers/notifiers/control_provider.dart';
 import 'package:stories_editor/src/domain/providers/notifiers/draggable_widget_notifier.dart';
 import 'package:stories_editor/src/domain/providers/notifiers/painting_notifier.dart';
 import 'package:stories_editor/src/domain/providers/notifiers/text_editing_notifier.dart';
+import 'package:stories_editor/src/domain/providers/notifiers/scroll_notifier.dart';
 import 'package:stories_editor/src/domain/sevices/save_as_image.dart';
 import 'package:stories_editor/src/presentation/utils/Extensions/hexColor.dart';
 import 'package:stories_editor/src/presentation/utils/constants/app_enums.dart';
 import 'package:stories_editor/src/presentation/widgets/animated_onTap_button.dart';
+import 'package:stories_editor/src/presentation/widgets/circularButtonDesign.dart';
 
 /// create item of type GIF
 Future createGiphyItem(
     {required BuildContext context,
     required String giphyKey,
+    required Widget? addMediaTopWidget,
     String? giphyRating,
     String? giphyLanguage}) async {
   final _editableItem =
@@ -30,6 +35,8 @@ Future createGiphyItem(
     crossAxisCount: 3,
     childAspectRatio: 1.2,
     topDragColor: Colors.white.withOpacity(0.2),
+    //
+    addMediaTopWidget: addMediaTopWidget,
   );
 
   /// create item of type GIF
@@ -39,6 +46,118 @@ Future createGiphyItem(
       ..gif = _editableItem.giphy!
       ..position = const Offset(0.0, 0.0));
   }
+}
+
+//TODO - do!
+////---add GifView package
+/*
+GifView.asset(
+                'assets/onBoarding1-2.gif',
+                onFrame: (value) =>  duration = ((value/15)*pow(10, 3)).round(),
+                onFinish: () => defaultDuration = duration;
+              ),
+*/
+Future<Duration> extractGifDuration(String path) async {
+  ////final gifPath = "assets/gif_path.gif";
+  Duration duration = Duration.zero;
+  // ui.ImmutableBuffer buffer = await ui.ImmutableBuffer.fromAsset(path);
+  ui.ImmutableBuffer buffer = await ui.ImmutableBuffer.fromFilePath(path);
+  ui.Codec codec =
+      await PaintingBinding.instance.instantiateImageCodecWithSize(buffer);
+  for (var i = 0; i < codec.frameCount; i++) {
+    ui.FrameInfo frameInfo = await codec.getNextFrame();
+    duration += frameInfo.duration;
+  }
+  return duration;
+}
+
+double GetRandomNumber(double minimum, double maximum) {
+  math.Random random = new math.Random();
+  return random.nextDouble() * (maximum - minimum) + minimum;
+}
+
+showAddMoreMedia(BuildContext context, ScrollNotifier scrollNotifier,
+    {required Widget addMediaTopWidget, required bool whiteTheme}) async {
+  await showModalBottomSheet(
+      context: context,
+      barrierColor: Colors.transparent,
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return FractionallySizedBox(
+          heightFactor: 0.3,
+          child: DraggableScrollableSheet(
+            initialChildSize: 0.9,
+            maxChildSize: 1,
+            minChildSize: 0.9,
+            expand: true,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(16),
+                          topLeft: Radius.circular(16)),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 16,
+                            color: (whiteTheme ? Colors.white : Colors.black)
+                                .withOpacity(0.3),
+                            offset: const Offset(0, 16))
+                      ]),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(16),
+                        topLeft: Radius.circular(16)),
+                    child: BackdropFilter(
+                      filter: ui.ImageFilter.blur(
+                        sigmaX: 40.0,
+                        sigmaY: 40.0,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: (whiteTheme ? Colors.white : Colors.black)
+                                .withOpacity(0.5),
+                            borderRadius: const BorderRadius.only(
+                                topRight: Radius.circular(16),
+                                topLeft: Radius.circular(16)),
+                            border: Border.all(
+                              width: 1.5,
+                              color: Colors.transparent,
+                            )),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Container(
+                                  width: 40,
+                                  height: 5,
+                                  decoration: BoxDecoration(
+                                      color: whiteTheme
+                                          ? Colors.grey
+                                          : Colors.white54,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(10))),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 40,
+                              ),
+                              addMediaTopWidget,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      });
 }
 
 /// custom exit dialog
@@ -56,14 +175,14 @@ Future<bool> exitDialog({
         barrierDismissible: true,
         builder: (BuildContext dialogContext) => Dialog(
           backgroundColor: Colors.transparent,
-          elevation: 0,
+          elevation: 0.7,
           insetAnimationDuration: const Duration(milliseconds: 300),
           insetAnimationCurve: Curves.ease,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Container(
               padding: const EdgeInsets.only(
-                  top: 25, bottom: 5, right: 20, left: 20),
+                  top: 25, bottom: 5, right: 15, left: 15),
               alignment: Alignment.center,
               height: 280,
               decoration: BoxDecoration(
@@ -142,7 +261,7 @@ Future<bool> exitDialog({
                               if (recordCallback != null &&
                                   (_widgetProvider.draggableWidget.indexWhere(
                                           (element) =>
-                                         element.animationType !=
+                                              element.animationType !=
                                                   TextAnimationType.none ||
                                               element.type == ItemType.gif ||
                                               element.type == ItemType.video ||
