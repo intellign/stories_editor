cdd75c4262224cf8869ba5da1ee4c157b9ebf5c6
intellign <ismailarashad@aol.com>
2024-12-18 19:23:53 -0500

    Update editable_items.dart

--- lib/src/domain/models/editable_items.dart
+++ lib/src/domain/models/editable_items.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:modal_gif_picker/modal_gif_picker.dart';
+import 'package:giphy_get/giphy_get.dart';
 import 'package:stories_editor/src/presentation/utils/constants/app_enums.dart';
 
 class EditableItem {
