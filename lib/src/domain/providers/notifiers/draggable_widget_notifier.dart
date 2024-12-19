a281fbe2a16cde1f4edf18dee3307e29b2d0a17d
intellign <ismailarashad@aol.com>
2024-12-18 19:23:30 -0500

    Update draggable_widget_notifier.dart

--- lib/src/domain/providers/notifiers/draggable_widget_notifier.dart
+++ lib/src/domain/providers/notifiers/draggable_widget_notifier.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:modal_gif_picker/modal_gif_picker.dart';
+import 'package:giphy_get/giphy_get.dart';
 import 'package:stories_editor/src/domain/models/editable_items.dart';
 
 class DraggableWidgetNotifier extends ChangeNotifier {
